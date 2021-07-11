bf6eb6b13abb90d0784e7112d1cded0b246075b1
Asocia <akkayas17@itu.edu.tr>
2021-07-11 16:51:11 +0300

    Switch to i3

---
+++ scripts/misc/battery-level.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Set path for running in a cron job
+PATH='/snap/bin:/home/sahin/.poetry/bin:/home/sahin/.pyenv/bin:/home/sahin/.pyenv/shims:/home/sahin/.pyenv/bin:/home/sahin/.cargo/bin:/home/sahin/.poetry/bin:/home/sahin/.local/bin:/home/sahin/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin:/home/sahin/Downloads/ArchiveFiles/idea-IU-211.7628.21/bin:/home/sahin/scripts:/home/sahin/.fzf/bin'
+
+# Set limit to shell argument
+batteryLimit=$1
+
+export DISPLAY=:0
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
+# Get the current battery level with acpi
+batteryLevel=$(acpi | cut -d ' ' -f 4 | tr -d '%|,| ')
+charging=$(acpi | cut -d ' ' -f 3 | tr -d ',| ')
+
+# Must have the Xdus file to run in a cron job. If this is not
+# necessary functionality, this can be ignored, otherwise one must
+# run the /scripts/xdbus.sh script in this repository at startup,
+# which is called by default from my i3/config file.
+# if [ -r "$HOME/.dbus/Xdbus" ]; then
+#   . "$HOME/.dbus/Xdbus"
+# fi
+
+if [[ "$batteryLevel" -le "$batteryLimit" ]] && [[ "$charging" == "Discharging" ]]; then
+    /usr/bin/notify-send -a "Battery Warning" --urgency critical "Battery level at ${batteryLevel}%" -i abrt
+    /usr/bin/paplay /usr/share/sounds/Oxygen-K3B-Finish-Error.ogg
+fi
---
+++ scripts/rofi/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+menu="$1"
+
+if [ "$menu" = "music" ]; then
+    rofi -modi 'Music:~/scripts/rofi/music.sh' -show Music -theme music-launcher
+elif [ "$menu" = "appmenu" ]; then
+    rofi -show drun -theme clean
+elif [ "$menu" = "powermenu" ]; then
+    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -24 -yoffset 70
+fi
