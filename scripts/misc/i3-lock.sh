8ea02c9dd13368ec702fef08f40b83401ba84d0d
Asocia <akkayas17@itu.edu.tr>
2021-07-17 23:14:16 +0300

    Configure i3lock-color

---
+++ scripts/misc/i3-lock.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+# D='#ff00ffcc'  # default
+# T='#ee00eeee'  # text
+# W='#880000bb'  # wrong
+# V='#bb00bbbb'  # verifying
+
+D="$(xrdb -query | grep "*.color12\b" | cut -d":" -f2 | xargs)cc"
+T="$(xrdb -query | grep "*.color13\b" | cut -d":" -f2 | xargs)ee"
+W="$(xrdb -query | grep "*.color1\b" | cut -d":" -f2 | xargs)bb"
+V="$(xrdb -query | grep "*.color6\b" | cut -d":" -f2 | xargs)bb"
+
+
+i3lock \
+--insidever-color=$C   \
+--ringver-color=$V     \
+\
+--insidewrong-color=$C \
+--ringwrong-color=$W   \
+\
+--inside-color=$B      \
+--ring-color=$D        \
+--line-color=$B        \
+--separator-color=$D   \
+\
+--verif-color=$T        \
+--wrong-color=$T        \
+--time-color=$T        \
+--date-color=$T        \
+--layout-color=$T      \
+--keyhl-color=$W       \
+--bshl-color=$W        \
+\
+--screen 1            \
+--blur 5              \
+--clock               \
+--indicator           \
+--time-str="%H:%M:%S"  \
+--date-str="%A, %m %Y" \
+--keylayout 1         \
