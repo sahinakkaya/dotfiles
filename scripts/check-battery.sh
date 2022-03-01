5630243ef14d44450329e656d4aad0a214907817
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-03-01 16:07:01 +0300

    Organize scripts

---
+++ scripts/check-battery.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set limit to shell argument
+batteryLimit=$1
+fullAt=92
+
+# Get the current battery level with acpi
+batteryLevel=$(acpi | cut -d ' ' -f 4 | tr -d '%|,| ')
+charging=$(acpi | cut -d ' ' -f 3 | tr -d ',| ')
+
+echo $batteryLevel
+
+if [[ "$batteryLevel" -le "$batteryLimit" ]] && [[ "$charging" == "Discharging" ]]; then
+    /usr/bin/notify-send -a "Battery Warning" --urgency critical "Battery level at ${batteryLevel}%" -i abrt -t 10000
+    /usr/bin/paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
+#elif [[ "$batteryLevel" -gt "$fullAt" ]] && [[ ! "$charging" == "Discharging" ]]; then
+#    /usr/bin/notify-send -a "Battery Warning" "Battery is full: ${batteryLevel}" -i abrt -t 10000
+#    /usr/bin/paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
+fi
