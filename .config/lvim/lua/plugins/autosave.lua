6514f4847931f0fdc26daa75ca8abe51830a8cb4
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-03-18 01:50:52 +0300

    Remove unused files

--- .config/lvim/lua/plugins/autosave.lua
+++
@@ -1,9 +0,0 @@
-require('autosave').setup({
-  enabled = vim.g.auto_save,
-  -- execution_message = 'autosaved at : ' .. vim.fn.strftime('%H:%M:%S'),
-  -- events = {'InsertLeave', 'TextChanged'},
-  -- conditions = {exists = true, filetype_is_not = {}, modifiable = true},
-  write_all_buffers = true,
-  on_off_commands = true,
-  clean_command_line_interval = 2500
-})
