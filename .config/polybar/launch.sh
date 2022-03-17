8d4925cff2568a98879e3ed1eca11abcc0711640
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-03-18 02:11:58 +0300

    Add polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload main &
+done
