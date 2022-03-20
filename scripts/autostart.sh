ed4235db4340285b97dba47ee36a0c49c631e3e4
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-03-20 20:00:58 +0300

    Use builtin bar of qtile instead of polybar

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -12,10 +12,4 @@ function run {
 nitrogen --save --set-zoom-fill ~/Pictures/Wallpapers/default.jpg
 run picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
-# Low battery notifier
-#~/.config/qtile/scripts/check_battery.sh & disown
-
-# Start welcome
-#run eos-welcome & disown
-$HOME/.config/polybar/launch.sh & disown
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
