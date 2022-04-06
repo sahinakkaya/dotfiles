a80154bb2d6e0d60a7a9bb9c12b670af0d5e6958
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-04-06 23:55:36 +0300

    Update picom configuration

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -11,7 +11,7 @@ function run {
 
 rm ~/.cache/workspaces
 nitrogen --save --set-zoom-fill ~/Pictures/Wallpapers/default.jpg
-run picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
+run picom --experimental-backends --vsync -b # should prevent screen tearing on most setups if needed
 
 eww -c ~/.config/eww/bar/ close-all
 eww -c ~/.config/eww/bar/ open bar
