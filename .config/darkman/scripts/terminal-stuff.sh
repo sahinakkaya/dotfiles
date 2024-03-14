735f278b4f793ea4c5c1098be021a4522405f53e
Şahin Akkaya <sahin@sahinakkaya.dev>
2024-03-14 11:16:56 +0300

    Update linux config

--- .config/darkman/scripts/terminal-stuff.sh
+++ .config/darkman/scripts/terminal-stuff.sh
@@ -7,14 +7,18 @@
 theme=$(darkman get)
 PATH="$HOME/.local/bin/:$PATH"
 
-if [ $theme == "dark" ]; then
+if [ "$theme" == "dark" ]; then
+  sed -i 's/kanagawa_light.conf/kanagawa.conf/g' "$HOME/.config/kitty/kitty.conf"
   sed -i 's/tokyonight_day/tokyonight_storm/g' "$HOME/.config/kitty/kitty.conf"
   sed -i 's/Light/Origin/g' "$HOME/.config/bat/config"
   sed -i 's/flat-remix-light/tokyo-storm/g' "$HOME/.config/btop/btop.conf"
+  sed -i "\$s/.*/include dark-256.theme/" "$HOME/.config/task/taskrc"
 else
+  sed -i 's/kanagawa.conf/kanagawa_light.conf/g' "$HOME/.config/kitty/kitty.conf"
   sed -i 's/tokyonight_storm/tokyonight_day/g' "$HOME/.config/kitty/kitty.conf"
   sed -i 's/Origin/Light/g' "$HOME/.config/bat/config"
   sed -i 's/tokyo-storm/flat-remix-light/g' "$HOME/.config/btop/btop.conf"
+  sed -i "\$s/.*/include light-256.theme/" "$HOME/.config/task/taskrc"
 fi
 
 kill -SIGUSR1 $(pgrep kitty)
